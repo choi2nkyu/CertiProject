8595dd9ae129a73ac0b5a86dec28cd4d8abcee4d
jxuxaxcxo <joaquin.vargaselio@gmail.com>
2019-05-07 20:48:52 -0400

    Funciones para anadir objetos al Store

---
+++ src/components/AddFunctions.js
@@ -0,0 +1,23 @@
+/*
+addIncome(){
+    this.$store.dispatch('addIncome', 
+    {name:"hey" + this.currentName, 
+    category: this.currentCategory,
+    amount: this.currentAmount});
+  };
+
+  addExpense(){
+    this.$store.dispatch('addExpense', 
+    {name: this.currentName, 
+    category: this.currentCategory,
+    amount: this.currentAmount});
+  };
+
+  addAccount(){
+    this.$store.dispatch('addExpense', 
+    {name: this.currentName, 
+    description: this.currentDescription});
+  };
+*/
+
+//Jalense los metodos a sus componentes para que entren en el Store
