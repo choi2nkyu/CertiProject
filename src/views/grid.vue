813f62d2cc96e6fec59677527d0245b2dd2cc37b
Paola Lizarraga <lizarragapk@gmail.com>
2019-05-07 20:51:56 -0400

    payito: created UI for reportes

---
+++ src/views/grid.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <b-row>
+        <b-col>
+            <p>HOLAAAAA</p>
+        </b-col>
+    </b-row>
+</div>
+    
+</template>
+
+<script>
+export default {
+    name: 'Grid'
+}
+</script>
+
