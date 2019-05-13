5d906d6c10ee08ae20ef2cb6077907740fd2bf9f
Eun Kyu Choi <choi2nkyu@gmail.com>
2019-05-13 00:25:03 -0400

    DEV: adding navigating functions between components

--- src/router.js
+++ src/router.js
@@ -1,39 +1,33 @@
-
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import Income from "./views/Income.vue";
 import Expense from "./views/Expense.vue";
+import Reportes from "./views/reportes.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'reportes',
+      path: "/",
+      name: "home",
       component: Home
     },
     {
-      path: '/reportes',
-      name: 'reportes',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: function () {
-        return import(/* webpackChunkName: 'about' */ './views/reportes.vue')
-      }
+      path: "/reportes",
+      name: "reportes",
+      component: Reportes
     },
     {
       path: "/income",
       name: "income",
-      component: Income     
-      
+      component: Income
     },
-      {
-        path: "/expense",
-        name: "expense",
-        component: Expense
-      }    
+    {
+      path: "/expense",
+      name: "expense",
+      component: Expense
+    }
   ]
-})
+});
--- src/views/reportes.vue
+++ src/views/reportes.vue
@@ -1,52 +1,122 @@
 <template>
-   <div>
-     <h1>Reportes</h1>
+  <div>
+    <h1>Reportes</h1>
     <div class="row">
-       <div class="col-lg-6">
-       <h2>Ingresos</h2>
-       <b-table striped hover :items="items" :fields="fields"></b-table>
+      <div class="col-lg-6">
+        <h2>Ingresos</h2>
+        <b-table striped hover :items="items" :fields="fields"></b-table>
 
-       <div>
-        <b-button variant="outline-primary" style="margin-left: 10px">Add</b-button>
-        <b-button variant="outline-primary" style="margin-left: 10px">Edit</b-button>
-        <b-button variant="outline-danger" style="margin-left: 10px">Delete</b-button>
-       </div>
-     </div>
-     <div class="col-lg-6">
-       <h2>Egresos</h2>
-       <b-table striped hover :items="items2" :fields="fields"></b-table>
+        <div>
+          <b-button
+            variant="outline-primary"
+            style="margin-left: 10px"
+            @click="navigateToIncome"
+          >Add</b-button>
+          <b-button
+            variant="outline-primary"
+            style="margin-left: 10px"
+            @click="navigateToIncome"
+          >Edit</b-button>
+          <b-button variant="outline-danger" style="margin-left: 10px">Delete</b-button>
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <h2>Egresos</h2>
+        <b-table striped hover :items="items2" :fields="fields"></b-table>
 
-      <div>
-        <b-button variant="outline-primary" style="margin-left: 10px">Add</b-button>
-        <b-button variant="outline-primary" style="margin-left: 10px">Edit</b-button>
-        <b-button variant="outline-danger" style="margin-left: 10px">Delete</b-button>
-       </div>
-     </div>
+        <div>
+          <b-button
+            variant="outline-primary"
+            style="margin-left: 10px"
+            @click="navigateToExpense"
+          >Add</b-button>
+          <b-button
+            variant="outline-primary"
+            style="margin-left: 10px"
+            @click="navigateToExpense"
+          >Edit</b-button>
+          <b-button variant="outline-danger" style="margin-left: 10px">Delete</b-button>
+        </div>
+      </div>
     </div>
-   </div>
-
+  </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-
-      fields: ['motivo', 'categoria', 'monto', 'cuenta'],
+      fields: ["motivo", "categoria", "monto", "cuenta"],
       items: [
-        { isActive: true, monto: 1000, motivo: 'Viaje', categoria: 'Cachorras', cuenta: 'Ahorros' },
-        { isActive: false, monto: 1500, motivo: 'Colegiatura', categoria: 'Hijos', cuenta: 'Gastos' },
-        { isActive: false, monto: 800, motivo: 'Porque si', categoria: 'Trabajo', cuenta: 'Ahorros' },
-        { isActive: true, monto: 100, motivo: 'Ahorritos', categoria: 'General', cuenta: 'Ahorros' }
+        {
+          isActive: true,
+          monto: 1000,
+          motivo: "Viaje",
+          categoria: "Cachorras",
+          cuenta: "Ahorros"
+        },
+        {
+          isActive: false,
+          monto: 1500,
+          motivo: "Colegiatura",
+          categoria: "Hijos",
+          cuenta: "Gastos"
+        },
+        {
+          isActive: false,
+          monto: 800,
+          motivo: "Porque si",
+          categoria: "Trabajo",
+          cuenta: "Ahorros"
+        },
+        {
+          isActive: true,
+          monto: 100,
+          motivo: "Ahorritos",
+          categoria: "General",
+          cuenta: "Ahorros"
+        }
       ],
 
       items2: [
-        { isActive: true, monto: 3000, motivo: 'Viaje PC', categoria: 'Cachorras', cuenta: 'Ahorros' },
-        { isActive: false, monto: 150, motivo: 'Mandarina', categoria: 'Diversion', cuenta: 'Ahorros' },
-        { isActive: false, monto: 900, motivo: 'Cachorreadas', categoria: 'Cachorras', cuenta: 'Ahorros' },
-        { isActive: true, monto: 80, motivo: 'Pa comer', categoria: 'General', cuenta: 'Ahorros' }
+        {
+          isActive: true,
+          monto: 3000,
+          motivo: "Viaje PC",
+          categoria: "Cachorras",
+          cuenta: "Ahorros"
+        },
+        {
+          isActive: false,
+          monto: 150,
+          motivo: "Mandarina",
+          categoria: "Diversion",
+          cuenta: "Ahorros"
+        },
+        {
+          isActive: false,
+          monto: 900,
+          motivo: "Cachorreadas",
+          categoria: "Cachorras",
+          cuenta: "Ahorros"
+        },
+        {
+          isActive: true,
+          monto: 80,
+          motivo: "Pa comer",
+          categoria: "General",
+          cuenta: "Ahorros"
+        }
       ]
+    };
+  },
+  methods: {
+    navigateToIncome() {
+      this.$router.push("income");
+    },
+    navigateToExpense() {
+      this.$router.push("expense");
     }
   }
-}
+};
 </script>
